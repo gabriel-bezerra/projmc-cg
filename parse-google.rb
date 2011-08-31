2798dd8e34f48fdae38aa0c69da7516f73679452
Gabriel Assis Bezerra <gabriel.bezerra@gmail.com>
2011-08-31 02:04:25 -0300

    create a google searcher from the passer methods

--- parse-google.rb
+++ parse-google.rb
@@ -2,66 +2,69 @@
 require 'nokogiri'
 require 'mechanize'
 
-# read results page
-def get_results_page
-    doc = nil
+class GoogleSearcher
 
-    File.open('search.htm') do |f|
-        doc = Nokogiri::HTML(f)
+    def search(query)
+        page = get_results_page(query)
+        results_section = get_results_section(page)
+
+        html_links = get_html_links(results_section)
+        clean_links(html_links)
+
+        to_links(html_links)
     end
 
-    doc
-end
+    private
 
-# get results div from results page
-def get_results_div(doc)
-    doc.css('div#ires ol')
-end
-
-# get links from results div
-def get_html_links(results_div)
-    result_links = []
-
-    results_div.css('a.l').each do |link|
-        result_links.push link
+    def get_results_page(query)
+        agent = Mechanize.new
+        agent.get("http://www.google.com/search?q=#{query}").parser
     end
 
-    result_links
-end
+    def get_results_section(results_page)
+        results_page.css('div#ires ol')
+    end
+
+    # get links from results section
+    def get_html_links(results_section)
+        result_links = []
+
+        results_section.css('a.l').each do |link|
+            result_links.push link
+        end
+
+        result_links
+    end
 
 
-# links must start with http://
-def clean_links(html_links)
-    html_links.each do |node|
-        unless /^http:\/\// =~ node.attribute('href')
-            actual_url = node.attribute('href').content.sub(/^.*http:\/\//, 'http://')
-            node.attribute('href').content = actual_url
+    # links must start with http://
+    def clean_links(html_links)
+        html_links.each do |node|
+            unless /^http:\/\// =~ node.attribute('href')
+                actual_url = node.attribute('href').content.sub(/^.*http:\/\//, 'http://')
+                node.attribute('href').content = actual_url
+            end
         end
     end
-end
 
 
-# creates Mechanize::Page::Links because it is easier to parse
-def to_links(html_links)
-    links = []
+    # creates Mechanize::Page::Links because it is easier to parse
+    def to_links(html_links)
+        links = []
 
-    html_links.each do |link_node|
-       links.push Mechanize::Page::Link.new(link_node, nil, nil)
+        html_links.each do |link_node|
+           links.push Mechanize::Page::Link.new(link_node, nil, nil)
+        end
+
+        links
     end
 
-    links
 end
 
-
-page = get_results_page()
-div = get_results_div(page)
-html_links = get_html_links(div)
-clean_links(html_links)
-result_links = to_links(html_links)
-
 # print results
-result_links.each do |link|
+GoogleSearcher.new.search('query').each do |link|
     puts link.text
     puts link.href
     puts
 end
+
