b7b0c61ae3aaf142967970ffd65aa5dfdd1ecd1c
Gabriel Assis Bezerra <gabriel.bezerra@gmail.com>
2011-08-31 01:22:05 -0300

    filter some dirty results url, add some doc to the parser

--- parse-google.rb
+++ parse-google.rb
@@ -2,13 +2,18 @@
 require 'nokogiri'
 require 'mechanize'
 
-f = File.open('search.htm')
-doc = Nokogiri::HTML(f)
-f.close
+# read results page
+doc = nil
+
+File.open('search.htm') do |f|
+    doc = Nokogiri::HTML(f)
+end
 
 
+# get results div from results page
 results_div = doc.css('div#ires ol')
 
+# get links from results div
 result_links = []
 
 results_div.css('a.l').each do |link|
@@ -16,12 +21,26 @@ results_div.css('a.l').each do |link|
 end
 
 
+# links must start with http://
+result_links.each do |node|
+    unless /^http:\/\// =~ node.attribute('href')
+        actual_url = node.attribute('href').content.sub(/^.*http:\/\//, 'http://')
+        node.attribute('href').content = actual_url
+    end
+end
 
+
+# creates Mechanize::Page::Links because it is easier to parse
 links = []
 
 result_links.each do |link_node|
    links.push Mechanize::Page::Link.new(link_node, nil, nil)
 end
 
-puts links.to_s
 
+# print results
+links.each do |link|
+    puts link.text
+    puts link.href
+    puts
+end
