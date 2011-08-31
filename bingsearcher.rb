842eb7211eb803dadb2e1eced329a174d61f069e
Gabriel Assis Bezerra <gabriel.bezerra@gmail.com>
2011-08-31 05:15:02 -0300

    extract class refactoring - part 1 (bing)

--- bingsearcher.rb
+++ bingsearcher.rb
@@ -1,66 +1,23 @@
+require './searcher.rb'
 
-require 'nokogiri'
-require 'mechanize'
+class BingSearcher < Searcher
 
-class BingSearcher
-
-    def search_for(query)
-        extract_results_from results_page_for query
+    def initialize
+        @test_page = 'test-search-bing.htm'
     end
 
-    def test_search
-        extract_results_from test_results_page
+    private
+
+    def query_link(query)
+        "http://bing.com/search?q=#{query}"
     end
-
-    #private
-
-    def extract_results_from(page)
-        results_section = results_section_from(page)
-
-        html_links = html_links_from(results_section)
-
-        to_links(html_links)
-    end
-
-
-    def test_results_page
-        doc = nil
-        File.open('test-search-bing.htm') do |file|
-            doc = Nokogiri.HTML(file)
-        end
-        doc
-    end
-
-    def results_page_for(query)
-        agent = Mechanize.new
-        agent.get("http://bing.com/search?q=#{query}").parser
-    end
-
 
     def results_section_from(results_page)
         results_page.css('div #results ul')
     end
 
     def html_links_from(results_section)
-        result_links = []
-
-        results_section.css('li.sa_wr div.sb_tlst h3').css('a').each do |link|
-            result_links.push link
-        end
-
-        result_links
-    end
-
-    # creates Mechanize::Page::Links from the html links because it is easier to
-    # use
-    def to_links(html_links)
-        links = []
-
-        html_links.each do |link_node|
-           links.push Mechanize::Page::Link.new(link_node, nil, nil)
-        end
-
-        links
+        results_section.css('li.sa_wr div.sb_tlst h3').css('a')
     end
 
 end
---
+++ searcher.rb
@@ -0,0 +1,50 @@
+
+require 'nokogiri'
+require 'mechanize'
+
+class Searcher
+
+    def search_for(query)
+        extract_results_from results_page_for query
+    end
+
+    def test_search
+        extract_results_from test_results_page
+    end
+
+    private
+
+    def extract_results_from(page)
+        results_section = results_section_from(page)
+
+        html_links = html_links_from(results_section)
+
+        to_links(html_links)
+    end
+
+    def test_results_page
+        doc = nil
+        File.open(@test_page) do |file|
+            doc = Nokogiri.HTML(file)
+        end
+        doc
+    end
+
+    def results_page_for(query)
+        agent = Mechanize.new
+        agent.get(query_link(query)).parser
+    end
+
+    # creates Mechanize::Page::Links from the html links because it is easier to
+    # use
+    def to_links(html_links)
+        links = []
+
+        html_links.each do |link_node|
+           links.push Mechanize::Page::Link.new(link_node, nil, nil)
+        end
+
+        links
+    end
+
+end
