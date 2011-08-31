0aaa641d328b8a3b58d61b23ce8efa4ac846978a
Gabriel Assis Bezerra <gabriel.bezerra@gmail.com>
2011-08-31 16:55:59 -0300

    refactor yahoo engine

--- yahoosearcher.rb
+++ yahoosearcher.rb
@@ -1,8 +1,6 @@
-require './searcher'
+require './engine'
 
-class YahooSearcher < Searcher
-
-    private
+class YahooEngine < Engine
 
     def test_page
         'test-search-yahoo.htm'
@@ -12,20 +10,21 @@ class YahooSearcher < Searcher
         "http://search.yahoo.com/search?p=#{query}"
     end
 
-    def results_section_from(results_page)
-        results_page.css('div #web ol')
-    end
+    def html_links_from(results_page)
+        results_section = results_page.css('div #web ol')
 
-    def html_links_from(results_section)
         results_section.css('a.yschttl')
     end
 
 end
 
+
 """
+require './searcher'
+
 # print results
-#YahooSearcher.new.search_for('query').each do |link|
-YahooSearcher.new.test_search.each do |link|
+#Searcher.new(YahooEngine.new).search_for('query').each do |link|
+Searcher.new(YahooEngine.new).test_search.each do |link|
     puts link.text
     puts link.href
     puts
