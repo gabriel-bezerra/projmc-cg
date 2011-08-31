830687da54cf293b3a434904000c32b2f9d59c47
Gabriel Assis Bezerra <gabriel.bezerra@gmail.com>
2011-08-31 05:32:32 -0300

    extract class refactoring - part 3 (google) + fix google search problem that catched the wrong results

--- googlesearcher.rb
+++ googlesearcher.rb
@@ -1,79 +1,23 @@
+require './searcher'
 
-require 'nokogiri'
-require 'mechanize'
+class GoogleSearcher < Searcher
 
-class GoogleSearcher
-
-    def search_for(query)
-        extract_results_from results_page_for query
-    end
-
-    def test_search
-        extract_results_from test_results_page
+    def initialize
+        @test_page = 'test-search-google.htm'
     end
 
     private
 
-    def extract_results_from(page)
-        results_section = results_section_from(page)
-
-        html_links = html_links_from(results_section)
-        clean_links(html_links)
-
-        to_links(html_links)
+    def query_link(query)
+        "http://www.google.com/search?q=#{query}"
     end
-
-
-    def test_results_page
-        doc = nil
-        File.open('test-search-google.htm') do |file|
-            doc = Nokogiri.HTML(file)
-        end
-        doc
-    end
-
-    def results_page_for(query)
-        agent = Mechanize.new
-        agent.get("http://www.google.com/search?q=#{query}").parser
-    end
-
 
     def results_section_from(results_page)
         results_page.css('div #ires ol')
     end
 
     def html_links_from(results_section)
-        result_links = []
-
-        results_section.css('a.l').each do |link|
-            result_links.push link
-        end
-
-        result_links
-    end
-
-    # links must start with http:// if it has http://
-    def clean_links(html_links)
-        html_links.each do |node|
-            unless /^http:\/\// =~ node.attribute('href') \
-                    and /http:\/\// =~ node.attribute('href')
-                actual_url = node.attribute('href').content.sub(/^.*http:\/\//,
-                                                                'http://')
-                node.attribute('href').content = actual_url
-            end
-        end
-    end
-
-    # creates Mechanize::Page::Links from the html links because it is easier to
-    # use
-    def to_links(html_links)
-        links = []
-
-        html_links.each do |link_node|
-           links.push Mechanize::Page::Link.new(link_node, nil, nil)
-        end
-
-        links
+        results_section.css('h3.r a.l')
     end
 
 end
