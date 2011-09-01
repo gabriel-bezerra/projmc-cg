008efd16fb88411ba7ada58a4f6150975889eedf
Gabriel Assis Bezerra <gabriel.bezerra@gmail.com>
2011-09-01 04:04:29 -0300

    remove unused files from repository

--- rubygems.sh
+++
@@ -1,8 +0,0 @@
-wget http://production.cf.rubygems.org/rubygems/rubygems-1.8.7.zip
-unzip rubygems-1.8.7.zip
-cd rubygems-1.8.7
-ruby setup.rb --prefix=~/gems/
-
-cd ..
-rm -r rubygems-1.8.7 rubygems-1.8.7.zip
-
