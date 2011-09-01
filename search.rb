ae00f81214a5be50f332d23cd6e0d99031e7c358
Gabriel Assis Bezerra <gabriel.bezerra@gmail.com>
2011-09-01 00:56:02 -0300

    some refactoring and serialization code

--- search.rb
+++ search.rb
@@ -6,9 +6,10 @@ require './engines/yahooengine'
 require './engines/bingengine'
 require './engines/proxiedengine'
 
+
 class Result
     attr_reader :title, :url, :rank, :page
-    
+
     def initialize title, url, rank, page
         @title = title
         @url = url
@@ -17,7 +18,81 @@ class Result
     end
 end
 
+class Scraper
 
+    def initialize
+        @searchers = {#:google => Searcher.new(GoogleEngine.new),
+                      #:yahoo => Searcher.new(YahooEngine.new),
+                      :bing => Searcher.new(ProxiedEngine.new(BingEngine.new))}
+
+        @queries = [#'google bought motorola',
+                    #'george bush planned 9/11 atacks',
+                    'when did titanic sank?']
+    end
+
+    def showtime
+        all_results = {}
+
+        @queries.each do |query|
+            query_results = {}
+
+            @searchers.each do |symbol, searcher|
+                query_results[symbol] = results_from searcher.search_for query
+            end
+
+            all_results[query] = query_results
+        end
+
+        all_results
+    end
+
+    def results_from(ordered_links)
+        ordered_links.map do |link|
+            Result.new link.text,
+                       link.href,
+                       ordered_links.index(link),
+                       fetch_page_from(link)
+        end
+    end
+
+    def fetch_page_from link
+        agent = Mechanize.new
+        agent.user_agent_alias = 'Linux Firefox'
+
+        agent.get(link.href).body
+    end
+
+end
+
+#TODO persist all_results
+#pp all_results
+pp Scraper.new.showtime
+
+require "yaml"
+
+def save(output_file, obj)
+    dump = YAML::dump(obj)
+
+    File.open(output_file, "w") do |file|
+        file.puts dump
+    end
+end
+
+def retrieve(input_file)
+    File.open(input_file) do |file|
+        return YAML::load file.read
+    end
+end
+
+"""
+file_name = 'queries.yaml'
+save(file_name, queries)
+q = retrieve(file_name)
+"""
+
+
+#Testing-----------------------------------------------------------------------
+"""
 def show_these_links(links)
     links.each do |link|
         puts link.text
@@ -26,45 +101,6 @@ def show_these_links(links)
     end
 end
 
-searchers = {:google => Searcher.new(GoogleEngine.new),
-             :yahoo  => Searcher.new(YahooEngine.new),
-             :bing   => Searcher.new(ProxiedEngine.new(BingEngine.new))}
-
-queries = [#'google bought motorola',
-           #'george bush planned 9/11 atacks',
-           'when did titanic sank?']
-
-all_results = {}
-
-def fetch_page link
-    agent = Mechanize.new
-    agent.user_agent_alias = 'Linux Firefox'
-
-    agent.get(link.href).body
-end
-
-queries.each do |query|
-    query_results = {}
-    
-    searchers.each do |symbol, searcher|
-        results = []
-        
-        links = searcher.search_for query
-        links.each do |link|
-            results.push Result.new link.text, link.href, links.index(link), fetch_page(link)
-        end
-        
-        query_results[symbol] = results
-    end
-    
-    all_results[query] = query_results
-end
-
-#TODO persist all_results
-#pp all_results
-
-#Testing-----------------------------------------------------------------------
-"""
 query = 'inheritance in ruby'
 
 puts 'GOOGLE------------------------------------------------------------------'
