54acd1059a659782762c9b68cfa56a3cfdcf6e7c
Cleber Gonçalves de Sousa <cgs.ccc@gmail.com>
2011-09-01 22:30:53 -0300

    all queries

--- collectdata.rb
+++ collectdata.rb
@@ -14,7 +14,20 @@ queries = ['google bought motorola',
            'johnson bros england history',
            'manolo blahnik shoes',
            'internet traffic chart web dead',
-           'documentary health care united states michael moore']
+           'documentary health care united states michael moore',
+	   'inheritance versus mixins in ruby',
+	   'web scraping ruby mechanize',
+	   'how to handle exceptions in java',
+	   'google web toolkit json rpc',
+	   'how to extrude a surface in autocad',
+	   'anonymous attacks visa mastercard wikileaks',
+	   'sony psn outage 2011',
+	   'space debris shuttle damage',
+	   'bin laden dead pictures leaked',
+	   'marvel avengers movie premiere',
+	   'khan academy ted talk',
+	   'richard dawkins the virus of faith',
+	   'richard dawkins the blind watchmaker']
 
 scraping_results = Scraper.new(queries).showtime
 
