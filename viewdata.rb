0989750bd4af3a9df1402b808eece6b7b76c4185
Gabriel Assis Bezerra <gabriel.bezerra@gmail.com>
2011-10-03 14:34:24 -0300

    creates a method that does the synthesis of the query results onto engine results

--- viewdata.rb
+++ viewdata.rb
@@ -5,20 +5,34 @@ require "./marshalrepository"
 repository = MarshalRepository.new 'collected-data'
 search_results_by_query = repository.retrieve
 
-search_results_by_engine = {}
-search_results_by_engine.default = []
+# parameter:
+#   search_results_by_query {query => {:engine => [res1, res2, ...]}}
+#
+# return:
+#   {:engine => [res1, res2, ...]} with all the results for all the queries in the
+#   engine's array of results
+#
+def search_results_by_engine_from search_results_by_query
+    results_by_engine = {}
+    results_by_engine.default = []
 
-search_results_by_query.each do |query, engine_results|
-    engine_results.each do |engine, results|
-        search_results_by_engine[engine] += results
+    search_results_by_query.each do |query, engine_results|
+
+        engine_results.each do |engine, results|
+            results_by_engine[engine] += results
+        end
     end
+
+    results_by_engine
 end
 
 
-search_results_by_engine.each do |engine, results|
+search_results_by_engine_from(search_results_by_query).each do |engine, results|
     puts "#{engine}: #{results.length}"
 
     results.each do |r|
         puts "\t#{r.rank} - #{r.title} - #{r.url}"
     end
 end
+
+
