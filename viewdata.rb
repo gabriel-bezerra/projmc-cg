da07c5794b984c613fc08e0e26d7d4952ceca018
Gabriel Assis Bezerra <gabriel.bezerra@gmail.com>
2011-10-03 15:19:58 -0300

    Plotting a bar chart with the total number of results per engine

--- viewdata.rb
+++ viewdata.rb
@@ -2,8 +2,11 @@
 
 require "./marshalrepository"
 
+# load collected data
+
 repository = MarshalRepository.new 'collected-data'
 search_results_by_query = repository.retrieve
+
 
 # parameter:
 #   search_results_by_query {query => {:engine => [res1, res2, ...]}}
@@ -26,13 +29,30 @@ def search_results_by_engine_from search_results_by_query
     results_by_engine
 end
 
+results_by_engine = search_results_by_engine_from search_results_by_query
+
+
+# Bar chart with total amount of results per engine
+
+require "rinruby"
+
+R.assign "engines", results_by_engine.keys.map { |engine| engine.to_s }
+R.result_counts = results_by_engine.values.map { |results| results.length }
+
+R.eval <<EOF
+    png("total_results_per_engine.png")
+    names(result_counts) <- engines
+    barplot(result_counts, main="Total number of results per engine")
+EOF
+
+
+# stdout text with total amount of results per engine
 
 search_results_by_engine_from(search_results_by_query).each do |engine, results|
     puts "#{engine}: #{results.length}"
 
-    results.each do |r|
-        puts "\t#{r.rank} - #{r.title} - #{r.url}"
-    end
+#    results.each do |r|
+#        puts "\t#{r.rank} - #{r.title} - #{r.url}"
+#    end
 end
-
 
