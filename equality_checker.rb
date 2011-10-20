d2eccdd29b10a89dbdded1f31109855ec0caefae
Gabriel Assis Bezerra <gabriel.bezerra@gmail.com>
2011-10-20 14:00:55 -0300

    calculating the equality_ratio for each query

--- equality_checker.rb
+++ equality_checker.rb
@@ -7,14 +7,68 @@ require "./marshalrepository"
 repository = MarshalRepository.new 'collected-data'
 search_results_by_query = repository.retrieve
 
+def largest_result_list_of lists_of_results
+    largest = lists_of_results.first
+
+    lists_of_results.each do |list|
+        if list.length > largest.length
+            largest = list
+        end
+    end
+
+    largest
+end
+
+# Tests
+0.times do
+    puts largest_result_list_of([[],[],[]]) == []
+    puts largest_result_list_of([['a'],[],[]]) == ['a']
+    puts largest_result_list_of([['a'],['a', 'b'],[]]) == ['a', 'b']
+end
+
+
+def smallest_result_list_of lists_of_results
+    smallest = lists_of_results.first
+
+    lists_of_results.each do |list|
+        if list.length < smallest.length
+            smallest = list
+        end
+    end
+
+    smallest
+end
+
+# Tests
+0.times do
+    puts smallest_result_list_of([[],[],[]]) == []
+    puts smallest_result_list_of([['a'],[],[]]) == []
+    puts smallest_result_list_of([['a'],['a', 'b'],[]]) == []
+end
+
+
 1.times do
     compared_engines = [:bing, :yahoo]
 
-    search_results_by_query.each do |key, value|
+    search_results_by_query.each do |query, engines|
         # Stub
-        puts key
-        puts value.keys
+        engine1s_results = engines[compared_engines.first]
+        engine2s_results = engines[compared_engines.last]
+
+        largest_result_list = largest_result_list_of [engine1s_results, engine2s_results]
+        smallest_result_list = smallest_result_list_of [engine1s_results, engine2s_results]
+
+        number_of_equal_results = 0
+        smallest_result_list.each_with_index do |result, index|
+            if result.url == largest_result_list[index].url
+                number_of_equal_results += 1
+            end
+        end
+
+        equality_ratio = (1.0 * number_of_equal_results) / largest_result_list.length
+
+        puts query,equality_ratio
+        puts
     end
 end
-
 
